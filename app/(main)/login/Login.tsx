40a86cc56da8eda18123b1ad4644a059fa3c6fd0
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-01-08 19:17:52 +0500

    login page changes

--- app/(main)/login/Login.tsx
+++ app/(main)/login/Login.tsx
@@ -50,7 +50,7 @@ export const Login = () => {
 
   return (
     <section className="bg-gray-100">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow-xl md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
