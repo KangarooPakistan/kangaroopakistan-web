593d31f8e7487fa593da80759d2df489a6d823ce
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2024-01-13 02:46:22 +0500

    auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import NextAuth, { type NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt'
   },
