1f76aacd988f15595ea577585757e7fb95012bc7
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-01-22 17:07:39 +0500

    viewcontest page code updated, profile page design updated

--- app/(routes)/admin/contesttypes/[id]/viewcontest/page.tsx
+++ app/(routes)/admin/contesttypes/[id]/viewcontest/page.tsx
@@ -1,30 +1,38 @@
 "use client";
-import { Contest, columns } from "./columns";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "next/navigation";
 import { DataTable } from "./data-table";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { Contest, columns } from "./columns";
 
-const ViewAllContests = () => {
-  const [contests, setContests] = useState([]);
+const ViewAllContests: React.FC = () => {
+  const [contests, setContests] = useState<Contest[]>([]);
   const params = useParams();
 
   useEffect(() => {
     async function fetchContestTypes() {
       try {
         const contestTypeId = params.id;
-        await axios
-          // .get(`/api/users/contests/active?contestTypeId=${contestTypeId}`) // show at user side
-          .get(`/api/users/contests?contestTypeId=${contestTypeId}`) // show at admin side
-          .then((resp) => {
-            console.log(resp.data);
-            setContests(resp.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+        const resp = await axios.get<Contest[]>(
+          `/api/users/contests?contestTypeId=${contestTypeId}`
+        );
+        const parsedContests = resp.data.map((contest) => {
+          const startDate = new Date(contest.startDate);
+          const endDate = new Date(contest.endDate);
 
-        // setIsLoading(false);
+          // Format start date as DD/MM/YY Day
+          const formattedStartDate = formatDate(startDate);
+
+          // Format end date as DD/MM/YY Day
+          const formattedEndDate = formatDate(endDate);
+
+          return {
+            ...contest,
+            startDate: formattedStartDate,
+            endDate: formattedEndDate,
+          };
+        });
+        setContests(parsedContests);
       } catch (error) {
         console.error("Error:", error);
       }
@@ -32,7 +40,21 @@ const ViewAllContests = () => {
 
     // Call the fetchContestTypes function when the component mounts
     fetchContestTypes();
-  }, []);
+  }, [params.id]);
+
+  // Helper function to format date as DD/MM/YY Day
+  const formatDate = (date: Date): string => {
+    return `${date.getDate().toString().padStart(2, "0")}/${(
+      date.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}/${date
+      .getFullYear()
+      .toString()
+      .slice(2)} ${new Intl.DateTimeFormat("en-US", {
+      weekday: "short",
+    }).format(date)}`;
+  };
 
   return (
     <div className="container mx-auto py-10">
--- app/(routes)/user/profile/[id]/page.tsx
+++ app/(routes)/user/profile/[id]/page.tsx
@@ -16,6 +16,23 @@ interface UserData {
   schoolId: number;
   schoolName: string;
   contactNumber: string;
+  schoolAddress: string;
+  district: string;
+  tehsil: string;
+  fax: string;
+  p_fName: string;
+  p_mName: string;
+  p_lName: string;
+  p_contact: string;
+  p_phone: string;
+  p_email: string;
+  c_fName: string;
+  c_mName: string;
+  c_lName: string;
+  c_contact: string;
+  c_phone: string;
+  c_email: string;
+  c_accountDetails: string;
   // Add other properties from your user data here
 }
 
@@ -28,6 +45,7 @@ function UserProfile({ params }: UserProfileProps) {
     axios
       .get(`/api/users/profile/${params.id}`)
       .then((response) => {
+        console.log(response.data);
         setUserData(response.data as UserData); // Set the fetched data to state
         setLoading(false);
       })
@@ -47,53 +65,140 @@ function UserProfile({ params }: UserProfileProps) {
 
   // Render the user profile using the userData state variable
   return (
-    <div>
-      <div className="flex h-screen w-full justify-center mt-20">
-        <div className="max-w-4xl">
-          <div className="bg-white shadow-xl rounded-lg py-4 px-40">
-            <div>
-              <img
-                className="w-32 h-32 rounded-full mx-auto"
-                src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg?w=740"
-                alt="profile"
-              />
+    <div className="flex  w-full justify-center mt-10 mb-10">
+      <div className="max-w-4xl">
+        <div className="bg-white shadow-xl rounded-lg py-4 px-40">
+          <div>
+            <img
+              className="w-32 h-32 rounded-full mx-auto"
+              src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg?w=740"
+              alt="profile"
+            />
+          </div>
+          <div>
+            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+              User Profile
+            </h3>
+            <div className="text-center text-gray-400 text-xs font-semibold">
+              <p>{userData.role}</p>
             </div>
-            <div>
-              <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-                User Profile
-              </h3>
-              <div className="text-center text-gray-400 text-xs font-semibold">
-                <p>{userData.role}</p>
-              </div>
-              <table className="text-xs my-3">
-                <tbody>
-                  <tr>
-                    <td className="px-2 py-2 text-gray-500 font-semibold">
-                      Email
-                    </td>
-                    <td className="px-2 py-2">{userData.email}</td>
-                  </tr>
-                  <tr>
-                    <td className="px-2 py-2 text-gray-500 font-semibold">
-                      School ID
-                    </td>
-                    <td className="px-2 py-2">{userData.schoolId}</td>
-                  </tr>
-                  <tr>
-                    <td className="px-2 py-2 text-gray-500 font-semibold">
-                      School Name
-                    </td>
-                    <td className="px-2 py-2">{userData.schoolName}</td>
-                  </tr>
-                  <tr>
-                    <td className="px-2 py-2 text-gray-500 font-semibold">
-                      Contact Number
-                    </td>
-                    <td className="px-2 py-2">{userData.contactNumber}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <table className="text-xs my-3">
+              <tbody>
+                <tr>
+                  <td colSpan={2} className="text-xl font-bold text-center">
+                    School Details
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Email
+                  </td>
+                  <td className="px-2 py-2">{userData.email}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    School ID
+                  </td>
+                  <td className="px-2 py-2">{userData.schoolId}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    School Name
+                  </td>
+                  <td className="px-2 py-2">{userData.schoolName}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Contact Number
+                  </td>
+                  <td className="px-2 py-2">{userData.contactNumber}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    School Address
+                  </td>
+                  <td className="px-2 py-2">{userData.schoolAddress}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    District
+                  </td>
+                  <td className="px-2 py-2">{userData.district}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Tehsil
+                  </td>
+                  <td className="px-2 py-2">{userData.tehsil}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">Fax</td>
+                  <td className="px-2 py-2">{userData.fax}</td>
+                </tr>
+                <tr>
+                  <td
+                    colSpan={2}
+                    className="text-xl font-bold text-center my-7"
+                  >
+                    Principal Details
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Principal Name
+                  </td>
+                  <td className="px-2 py-2">
+                    {userData.p_fName} {userData.p_mName} {userData.p_lName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Principal Email
+                  </td>
+                  <td className="px-2 py-2">{userData.p_email}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Principal Phone
+                  </td>
+                  <td className="px-2 py-2">{userData.p_phone}</td>
+                </tr>
+                <tr>
+                  <td
+                    colSpan={2}
+                    className="text-xl font-bold text-center my-7"
+                  >
+                    Coordinator Details
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Coordinator Name
+                  </td>
+                  <td className="px-2 py-2">
+                    {userData.c_fName} {userData.c_mName} {userData.c_lName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Coordinator Email
+                  </td>
+                  <td className="px-2 py-2">{userData.c_email}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Coordinator Phone
+                  </td>
+                  <td className="px-2 py-2">{userData.c_phone}</td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-500 font-semibold">
+                    Coordinator Account Details
+                  </td>
+                  <td className="px-2 py-2">{userData.c_accountDetails}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
