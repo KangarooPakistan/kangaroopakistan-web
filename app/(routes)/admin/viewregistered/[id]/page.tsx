f868fa1c34718e747fba0c3b7b4ad400af6fea6d
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-01-26 00:08:45 +0500

    updated admin viewregistered component

--- app/(routes)/admin/viewregistered/[id]/page.tsx
+++ app/(routes)/admin/viewregistered/[id]/page.tsx
@@ -7,9 +7,7 @@ import { DataTable } from "./data-table";
 import { Student, columns } from "./columns";
 
 interface ItemType {
-  // Define the properties of the item
   students: Student[];
-  // ... other properties if applicable
 }
 
 const ViewRegistered = () => {
@@ -23,19 +21,13 @@ const ViewRegistered = () => {
           `/api/users/contests/${params.id}/registrations`
         );
 
-        // Assuming registeredStudents.data is an array
         const data: ItemType[] = registeredStudents.data;
-        // console.log(data);
-        // Map through the array and update the state
+
         const mappedStudents = data.map((item: ItemType) => {
-          // Assuming item.students is an array of students
           return item.students;
         });
 
-        // Flatten the array if needed
         const flattenedStudents = mappedStudents.flat();
-
-        // Set the students state
         setStudents(flattenedStudents);
       } catch (error) {
         console.error("Error:", error);
