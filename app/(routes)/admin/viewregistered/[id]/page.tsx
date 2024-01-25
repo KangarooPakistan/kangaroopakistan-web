bf0c1ccdf3ce40732a32fae50a20a7a70b0571fc
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-01-25 23:58:48 +0500

    updated admin viewregistered component

--- app/(routes)/admin/viewregistered/[id]/page.tsx
+++ app/(routes)/admin/viewregistered/[id]/page.tsx
@@ -6,6 +6,12 @@ import React, { useEffect, useState } from "react";
 import { DataTable } from "./data-table";
 import { Student, columns } from "./columns";
 
+interface ItemType {
+  // Define the properties of the item
+  students: Student[];
+  // ... other properties if applicable
+}
+
 const ViewRegistered = () => {
   const [students, setStudents] = useState<Student[]>([]);
   const params = useParams();
@@ -16,18 +22,34 @@ const ViewRegistered = () => {
         const registeredStudents = await axios.get(
           `/api/users/contests/${params.id}/registrations`
         );
-        console.log(registeredStudents.data[0].students);
-        setStudents(registeredStudents.data[0].students);
+
+        // Assuming registeredStudents.data is an array
+        const data: ItemType[] = registeredStudents.data;
+        // console.log(data);
+        // Map through the array and update the state
+        const mappedStudents = data.map((item: ItemType) => {
+          // Assuming item.students is an array of students
+          return item.students;
+        });
+
+        // Flatten the array if needed
+        const flattenedStudents = mappedStudents.flat();
+
+        // Set the students state
+        setStudents(flattenedStudents);
       } catch (error) {
         console.error("Error:", error);
       }
     };
+
     fetch();
   }, []);
+
   return (
     <div className="container mx-auto py-10">
       <DataTable columns={columns} data={students} />
     </div>
   );
 };
+
 export default ViewRegistered;
