f22e9011db901829819603624631d7cf7faddbe8
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2024-01-25 22:23:09 +0500

    conflict resolved

--- app/(routes)/admin/viewregistered/[id]/page.tsx
+++ app/(routes)/admin/viewregistered/[id]/page.tsx
@@ -2,7 +2,6 @@
 import axios from "axios";
 import { getSession } from "next-auth/react";
 import { useParams } from "next/navigation";
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import { DataTable } from "./data-table";
 import { Student, columns } from "./columns";
@@ -14,19 +13,9 @@ const page = () => {
   useEffect(() => {
     const fetch = async () => {
       try {
-        const session = await getSession();
-
-        const response = await axios.get(
-          `/api/users/getuserbyemail/${session?.user.email}`
-        );
-        const regId = await axios.get(
-          `/api/users/contests/${params.id}/${response.data.schoolId}`
-        );
-        console.log(regId);
         const registeredStudents = await axios.get(
-          `/api/users/contests/${params.id}/registrations/${regId.data.id}`
+          `/api/users/contests/${params.id}/registrations`
         );
-
         console.log(registeredStudents);
         setStudents(registeredStudents.data);
       } catch (error) {
@@ -40,23 +29,5 @@ const page = () => {
       <DataTable columns={columns} data={students} />
     </div>
   );
-=======
-import React, { useEffect } from "react";
-
-const page = () => {
-  const params = useParams();
-  useEffect(() => {
-    const fetch = async () => {
-      const registeredStudents = await axios.get(
-        `/api/users/contests/${params.id}/registrations`
-      );
-
-      console.log(registeredStudents);
-    };
-    fetch();
-  }, []);
-  return <div>page</div>;
->>>>>>> a8041842edb0b2e738e604881ef1f5031816eb77
 };
-
 export default page;
