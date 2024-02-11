fb76267fc8242a33f97c2d183194e4ed393b0d2f
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2024-02-11 17:52:02 +0500

    student edit functionality

--- app/(routes)/user/viewregistered/[id]/[studentId]/page.tsx
+++ app/(routes)/user/viewregistered/[id]/[studentId]/page.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 import "react-toastify/dist/ReactToastify.css";
 import { toast, ToastContainer } from "react-toastify";
-import { Button } from "@/components/ui/button";
+
 import {
   Form,
   FormControl,
@@ -17,6 +17,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = zod.object({
   studentName: zod.string().min(1, "Student name is required"),
