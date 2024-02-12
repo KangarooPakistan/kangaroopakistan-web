9c4db8d00118c7a9b33e9a3600373e4057ae28dd
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2024-02-13 00:04:10 +0500

    debugging

--- app/api/users/fetchallregistrations/[id]/route.ts
+++ app/api/users/fetchallregistrations/[id]/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: Request, { params }: { params: { id: string } } )
         },
         include: {
           students: true,
-        
+          user:true // Include related students
       },
       });
       console.log(registrations)
