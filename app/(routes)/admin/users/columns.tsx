3d79d8959a6411dd8a18bb9a2f6dfe396bdbe9f3
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2024-02-14 13:56:06 +0500

    delete and user profile

--- app/(routes)/admin/users/columns.tsx
+++ app/(routes)/admin/users/columns.tsx
@@ -2,7 +2,17 @@
 
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
 export type User = {
   id: number;
   email: string;
@@ -11,6 +21,40 @@ export type User = {
   schoolName: string; // Optional: only for non-admin users
   contactNumber: string;
 };
+type UserActionsProps = {
+  user: User; // Use the Student type here
+};
+
+const RoleActions: React.FC<UserActionsProps> = ({ user }) => {
+  const router = useRouter();
+  const { onOpen } = useModal();
+
+  const handleView = () => {
+    console.log(user);
+    router.push(`/admin/userprofile/${user.id}`);
+  };
+  
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem onClick={handleView}>View School </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => onOpen("deleteSchool", { id: user.id })}
+        >
+          Delete School{" "}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
 export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "email",
@@ -51,5 +95,9 @@ export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "contactNumber",
     header: "contactNumber",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <RoleActions user={row.original} />,
   },
 ];
