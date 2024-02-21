2edaf6b5f0afb2bf482a454bb79287c8112a429f
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2024-02-21 19:23:41 +0500

    -school summary

--- app/(routes)/user/viewregistered/[id]/page.tsx
+++ app/(routes)/user/viewregistered/[id]/page.tsx
@@ -13,8 +13,16 @@ const ViewRegistered = () => {
   const { onOpen } = useModal();
   const router = useRouter();
   const [registrationId, setRegistrationId] = useState<string>();
+  const [preEculier, setPreEculier] = useState<number>(0);
+  const [totalPaymentDone, setTotalPaymentDone] = useState<number>(0);
+  const [eculier, setEculier] = useState<number>(0);
+  const [benjamin, setBenjamin] = useState<number>(0);
+  const [cadet, setCadet] = useState<number>(0);
+  const [junior, setJunior] = useState<number>(0);
+  const [student, setStudent] = useState<number>(0);
 
   const params = useParams();
+  type LevelCounts = Record<string, number>;
 
   useEffect(() => {
     const fetch = async () => {
@@ -24,14 +32,31 @@ const ViewRegistered = () => {
         const response = await axios.get(
           `/api/users/getuserbyemail/${session?.user.email}`
         );
-        
+
         const regId = await axios.get(
           `/api/users/contests/${params.id}/${response.data.schoolId}`
         );
+
         setRegistrationId(regId.data.id);
         const registeredStudents = await axios.get(
           `/api/users/contests/${params.id}/registrations/${regId.data.id}`
         );
+        console.log(registeredStudents);
+        const levelCounts = registeredStudents.data.reduce(
+          (acc: LevelCounts, student: Student) => {
+            const { level } = student;
+            acc[level] = (acc[level] || 0) + 1;
+            return acc;
+          },
+          {}
+        );
+        console.log(levelCounts);
+        setPreEculier(levelCounts["preecolier"] || 0);
+        setEculier(levelCounts["ecolier"] || 0);
+        setBenjamin(levelCounts["benjamin"] || 0);
+        setCadet(levelCounts["cadet"] || 0);
+        setJunior(levelCounts["junior"] || 0);
+        setStudent(levelCounts["student"] || 0);
 
         setStudents(registeredStudents.data);
       } catch (error) {
@@ -67,7 +92,36 @@ const ViewRegistered = () => {
           Attach Proof of Payment
         </Button>
       </div>
-      <div className="container mx-auto py-10">
+      <div className="flex justify-center items-center flex-wrap mt-4">
+        <div className="w-full md:w-1/2 px-2">
+          <div className="bg-gradient-to-r from-blue-500 to-teal-500 rounded-lg shadow-lg p-6 text-white transform transition duration-500 hover:scale-105">
+            <h2 className="font-bold text-2xl mb-4">Levels</h2>
+            <ul>
+              <li className="mb-2 text-lg font-medium">
+                Total # of Preecolier:{" "}
+                <span className="font-bold">{preEculier}</span>
+              </li>
+              <li className="mb-2 text-lg font-medium">
+                Total # of Ecolier: <span className="font-bold">{eculier}</span>
+              </li>
+              <li className="mb-2 text-lg font-medium">
+                Total # of Benjamin:{" "}
+                <span className="font-bold">{benjamin}</span>
+              </li>
+              <li className="mb-2 text-lg font-medium">
+                Total # of Cadet: <span className="font-bold">{cadet}</span>
+              </li>
+              <li className="mb-2 text-lg font-medium">
+                Total # of Junior: <span className="font-bold">{junior}</span>
+              </li>
+              <li className="mb-2 text-lg font-medium">
+                Total # of Student: <span className="font-bold">{student}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto py-5">
         <DataTable columns={columns} data={students} />
       </div>
     </>
