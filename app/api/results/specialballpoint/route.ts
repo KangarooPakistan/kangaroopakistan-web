9bc8ab9f9ac0bea7f73b83d5b5c10b7318e07398
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-05-20 04:25:23 +0500

    pdf for results

--- app/api/results/specialballpoint/route.ts
+++ app/api/results/specialballpoint/route.ts
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "Pakistan@123",
   database: "kangroo_db",
   connectionLimit: 10, // Set your desired connection limit
 });
