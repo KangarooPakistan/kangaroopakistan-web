79654b5cb0b49bbe7b5dc5b4dde2ce5acde8e7e5
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-05-20 19:46:28 +0500

    results

--- app/(routes)/user/viewregistered/[id]/page.tsx
+++ app/(routes)/user/viewregistered/[id]/page.tsx
@@ -240,13 +240,15 @@ const ViewRegistered = () => {
           </div>
         </div>
         <div className="flex flex-wrap justify-end mt-4">
-          <Link
-            href={pdfUrl}
-            className="w-full bg-purple-600 px-2 py-2 text-white sm:w-auto mx-2 my-1 sm:my-0"
-            target="_blank"
-            rel="noopener noreferrer">
-            View Results
-          </Link>
+          {pdfUrl ? (
+            <Button>
+              <Link href={pdfUrl} target="_blank" rel="noopener noreferrer">
+                View Results
+              </Link>
+            </Button>
+          ) : (
+            <Button>No results found</Button>
+          )}
 
           <Button
             className="w-full sm:w-auto mx-2 my-1 sm:my-0"
