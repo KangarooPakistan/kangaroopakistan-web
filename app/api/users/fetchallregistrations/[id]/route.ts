e78277a73189d3a755ef62d50ac831428b3b275f
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-06-10 23:58:19 +0500

    server side fetching api

--- app/api/users/fetchallregistrations/[id]/route.ts
+++ app/api/users/fetchallregistrations/[id]/route.ts
@@ -1,24 +1,34 @@
 import { NextResponse, NextRequest } from "next/server";
 import { db } from "@/app/lib/prisma";
+import zlib from "zlib";
 
-export async function GET(req: Request, { params }: { params: { id: string } } ){
-    try {
-      const registrations = await db.registration.findMany({
-        where: {
-          contestId: params.id
-        },
-        include: {
-          students: true,
-          paymentProof: true,
-          user:true // Include related students
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const registrations = await db.registration.findMany({
+      where: {
+        contestId: params.id,
       },
-      });
+      include: {
+        students: true,
+        paymentProof: true,
+        user: true, // Include related students
+      },
+    });
 
-      return NextResponse.json(registrations, { status: 200 });
+    const json = JSON.stringify(registrations);
+    const compressedData = zlib.gzipSync(json);
 
-    } catch (error:any) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
-
-    }
-  } 
-
+    return new NextResponse(compressedData, {
+      status: 200,
+      headers: {
+        "Content-Encoding": "gzip",
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
