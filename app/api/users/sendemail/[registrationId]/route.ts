06c78c7e6ae0bdf291057722dae4ab1a964d116d
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-07-20 18:04:46 +0500

    retry email function

--- app/api/users/sendemail/[registrationId]/route.ts
+++ app/api/users/sendemail/[registrationId]/route.ts
@@ -112,7 +112,7 @@ export async function GET(
     const sendEmailWithRetry = async (
       mailOptions: nodemailer.SendMailOptions,
       retries = 1
-    ) => {
+    ): Promise<{ success: boolean }> => {
       try {
         await transporter.sendMail(mailOptions);
         return { success: true };
