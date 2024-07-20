f012c5a2fcd9f5cc219cf467dc6b712517d64ec9
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-07-20 17:04:23 +0500

    some configs

--- app/lib/emailTransporter.ts
+++ app/lib/emailTransporter.ts
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtpout.secureserver.net",
+  host: "smtp.office364.com",
   port: 587,
   secure: false,
   connectionTimeout: 30000, // Reduced to 30 seconds
