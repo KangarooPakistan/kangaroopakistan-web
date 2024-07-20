cec393d8501dde5d7928d27075c57a416b7b6ead
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-07-20 16:04:49 +0500

    nodemailer config

--- app/lib/emailTransporter.ts
+++ app/lib/emailTransporter.ts
@@ -1,17 +1,17 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.office364.com",
+  host: "smtp.office365.com",
   port: 587,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.NODEMAILER_EMAIL,
     pass: process.env.NODEMAILER_PASS,
   },
-  connectionTimeout: 10000,
   tls: {
-    rejectUnauthorized: false,
     minVersion: "TLSv1.2",
+    ciphers:
+      "HIGH:MEDIUM:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!SRP:!CAMELLIA",
   },
 });
 
