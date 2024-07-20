7ad428b794ae78446bb13f5c1146794cf97e6325
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-07-20 18:01:19 +0500

    retry email function

--- app/api/users/sendemail/[registrationId]/route.ts
+++ app/api/users/sendemail/[registrationId]/route.ts
@@ -1,7 +1,6 @@
 import { db } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 import transporter from "@/app/lib/emailTransporter";
-
 import nodemailer from "nodemailer";
 
 export async function GET(
@@ -15,59 +14,40 @@ export async function GET(
         { status: 400 }
       );
     }
-    const registrations = await db.student.findMany({
-      where: {
-        registrationId: params.registrationId,
-      },
-    });
 
+    const registrations = await db.student.findMany({
+      where: { registrationId: params.registrationId },
+    });
     const registrationData = await db.registration.findFirst({
-      where: {
-        id: params.registrationId,
-      },
+      where: { id: params.registrationId },
     });
     const schoolDetails = await db.user.findFirst({
-      where: {
-        schoolId: registrationData?.schoolId,
-      },
+      where: { schoolId: registrationData?.schoolId },
     });
     const totalStudents = await db.student.findMany({
-      where: {
-        registrationId: params.registrationId,
-      },
+      where: { registrationId: params.registrationId },
     });
     const contestDate = await db.contest.findFirst({
-      where: {
-        id: registrationData?.contestId,
-      },
+      where: { id: registrationData?.contestId },
     });
-    let contestName;
+
+    let contestName, contestNameShort;
     switch (contestDate?.contestCh) {
       case "M":
         contestName = "International Kangaroo Mathematics Contest (IKMC)";
-        break;
-      case "S":
-        contestName = "International Kangaroo Science Contest (IKSC)";
-        break;
-      case "L":
-        contestName = "International Kangaroo Linguistic Contest (IKLC)";
-        break;
-      default:
-        contestName = "Contest Name Not Available"; // Handle unexpected values
-    }
-    let contestNameShort;
-    switch (contestDate?.contestCh) {
-      case "M":
         contestNameShort = "IKMC";
         break;
       case "S":
+        contestName = "International Kangaroo Science Contest (IKSC)";
         contestNameShort = "IKSC";
         break;
       case "L":
+        contestName = "International Kangaroo Linguistic Contest (IKLC)";
         contestNameShort = "IKLC";
         break;
       default:
-        contestNameShort = "Contest Name Not Available"; // Handle unexpected values
+        contestName = "Contest Name Not Available";
+        contestNameShort = "Contest Name Not Available";
     }
 
     if (!registrations) {
@@ -77,8 +57,7 @@ export async function GET(
       );
     }
 
-    let tableHtml = `
-    <table style="border: 1px solid #ddd; border-collapse: collapse;">
+    let tableHtml = `<table style="border: 1px solid #ddd; border-collapse: collapse;">
       <thead>
         <tr>
           <th style="border: 1px solid #ddd; padding: 5px 10px;">Roll Number</th>
@@ -91,84 +70,68 @@ export async function GET(
       <tbody>`;
 
     for (const student of totalStudents) {
-      tableHtml += `
-      <tr>
+      tableHtml += `<tr>
         <td style="border: 1px solid #ddd; padding: 5px 10px;">${student.rollNumber}</td>
         <td style="border: 1px solid #ddd; padding: 5px 10px;">${student.studentName}</td>
         <td style="border: 1px solid #ddd; padding: 5px 10px;">${student.fatherName}</td>
         <td style="border: 1px solid #ddd; padding: 5px 10px;">${student.class}</td>
         <td style="border: 1px solid #ddd; padding: 5px 10px;">${student.level}</td>
-      </tr>
-      `;
+      </tr>`;
     }
 
-    tableHtml += `</tbody>
-    </table>`;
-    try {
-      const startDateString = contestDate?.startDate;
-      let year = 0;
-      if (startDateString) {
-        const date = new Date(startDateString);
-        year = date.getFullYear();
-      } else {
-      }
+    tableHtml += `</tbody></table>`;
 
-      console.log(year);
-      await transporter.sendMail({
-        from: "info@kangaroopakistan.org",
-        to: schoolDetails?.email,
-        subject:
-          "Verification of Registration Details for " +
-          contestNameShort +
-          " " +
-          year,
-        html: `
-        <p><b>Dear Principal,</b></p>
-        <p>Congratulations on registering for the ${contestName} &nbsp; ${year}
-        </p>
-        <p>The contest will be held on ${contestDate?.contestDate} in your institute under your supervision.
-        </p>
-        <p>Below are the details of your institute. Please verify, as these details will be mentioned in all official documents:</p>
-        <p> School ID: ${schoolDetails?.schoolId}</p>
-        <p> School Name: ${schoolDetails?.schoolName}</p>
-        <p> School Address: ${schoolDetails?.schoolAddress}</p>
-        <p> Official Login Email Address: ${schoolDetails?.email}</p>
-        <p> Principal Name: ${schoolDetails?.p_Name}</p>
-        <p> Principal Email: ${schoolDetails?.p_email}</p>
-        <p> Principal Phone: ${schoolDetails?.p_phone}</p>
-        <p> Principal Cell: ${schoolDetails?.p_contact}</p>
-        <p> Coordinator Name: ${schoolDetails?.c_Name}</p>
-        <p> Coordinator Email: ${schoolDetails?.c_email}</p>
-        <p> Coordinator Phone: ${schoolDetails?.c_phone}</p>
-        <p> Coordinator Cell: ${schoolDetails?.c_contact}</p>
-        <p> Coordinator Account Details: ${schoolDetails?.c_accountDetails}</p>
-        <p> Total Number of students Registered: ${totalStudents.length}</p>
-        ${tableHtml}
-      `,
-      });
-    } catch (error) {
-      console.error("Error sending email:", error);
+    const startDateString = contestDate?.startDate;
+    let year = startDateString ? new Date(startDateString).getFullYear() : 0;
 
-      // Check if the error is an instance of Error and has a message property
-      if (error instanceof Error) {
-        return NextResponse.json(
-          {
-            error: `Failed to send email: ${error.message}`,
-            details: error.stack,
-          },
-          { status: 500 }
-        );
-      } else {
-        // Return a generic message if the error is not an instance of Error
-        return NextResponse.json(
-          { error: "An unknown error occurred while sending email" },
-          { status: 500 }
-        );
+    const mailOptions: nodemailer.SendMailOptions = {
+      from: "info@kangaroopakistan.org",
+      to: schoolDetails?.email,
+      subject: `Verification of Registration Details for ${contestNameShort} ${year}`,
+      html: `<p><b>Dear Principal,</b></p>
+      <p>Congratulations on registering for the ${contestName} ${year}</p>
+      <p>The contest will be held on ${contestDate?.contestDate} in your institute under your supervision.</p>
+      <p>Below are the details of your institute. Please verify, as these details will be mentioned in all official documents:</p>
+      <p> School ID: ${schoolDetails?.schoolId}</p>
+      <p> School Name: ${schoolDetails?.schoolName}</p>
+      <p> School Address: ${schoolDetails?.schoolAddress}</p>
+      <p> Official Login Email Address: ${schoolDetails?.email}</p>
+      <p> Principal Name: ${schoolDetails?.p_Name}</p>
+      <p> Principal Email: ${schoolDetails?.p_email}</p>
+      <p> Principal Phone: ${schoolDetails?.p_phone}</p>
+      <p> Principal Cell: ${schoolDetails?.p_contact}</p>
+      <p> Coordinator Name: ${schoolDetails?.c_Name}</p>
+      <p> Coordinator Email: ${schoolDetails?.c_email}</p>
+      <p> Coordinator Phone: ${schoolDetails?.c_phone}</p>
+      <p> Coordinator Cell: ${schoolDetails?.c_contact}</p>
+      <p> Coordinator Account Details: ${schoolDetails?.c_accountDetails}</p>
+      <p> Total Number of students Registered: ${totalStudents.length}</p>
+      ${tableHtml}`,
+    };
+
+    const sendEmailWithRetry = async (
+      mailOptions: nodemailer.SendMailOptions,
+      retries = 1
+    ) => {
+      try {
+        await transporter.sendMail(mailOptions);
+        return { success: true };
+      } catch (error) {
+        if (retries > 0) {
+          console.log(`Retrying to send email... Attempts left: ${retries}`);
+          return sendEmailWithRetry(mailOptions, retries - 1);
+        } else {
+          throw error;
+        }
       }
+    };
+
+    const emailResult = await sendEmailWithRetry(mailOptions, 1);
+    if (emailResult.success) {
+      return NextResponse.json("Email sent Successfully", { status: 200 });
     }
-
-    return NextResponse.json("Email sent Successfully", { status: 200 });
-  } catch (error) {
-    return NextResponse.json(params.registrationId, { status: 400 });
+  } catch (error: any) {
+    console.error("Error in GET function:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
