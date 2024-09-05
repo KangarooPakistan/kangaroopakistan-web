631669266f5fcedbe86b32ad03a1ca077fed2867
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-09-05 14:20:25 +0500

    add gtm in app

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import Head from "next/head";
+
 import AuthRedirect from "./components/AuthRedirect";
+import GoogleTM from "./components/GoogleTM";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +22,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <GoogleTM containerId="GTM-P4G559T8" />
+      </Head>
       <body className={inter.className}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P4G559T8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Providers>
           <ModalProvider />
           <AuthRedirect />
