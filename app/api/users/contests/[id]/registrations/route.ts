77e259a6965369dae1298c9c482e73ddd7d5a6bd
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-24 00:21:02 +0500

    -----

--- app/api/users/contests/[id]/registrations/route.ts
+++ app/api/users/contests/[id]/registrations/route.ts
@@ -235,10 +235,12 @@ export async function POST(
 
     const mailOptions: SendMailOptions = {
       to: schoolDetails?.email,
-      subject: `Verification of Registration Details for ${contestNameShort} ${year}`,
+      subject: `Confirmation of Registration ${contestNameShort} ${year}`,
       html: `<p><b>Dear Principal,</b></p>
-      <p>Congratulations on registering for the ${contestName} ${year}</p>
-      <p>The contest will be held on ${contestDate?.contestDate} in your institute under your supervision.</p>
+      <p>Thank you for registering your school for the ${contestName} ${year}. We have successfully received your registration details. Currently, we are in the process of verifying your payment to complete the registration procedure. </p>
+      <p>If there are any discrepancies or additional information required, our team will reach out to you promptly. In the meantime, if you have any questions or require assistance, please do not hesitate to contact us.</p>
+      <p>Please see attached list of students for any errors.</p>
+      <p>Thank you for your continued support and enthusiasm. We look forward to working together to make this year's contests a remarkable success.</p>
       <p>Below are the details of your institute. Please verify, as these details will be mentioned in all official documents:</p>
       <p> School ID: ${schoolDetails?.schoolId}</p>
       <p> School Name: ${schoolDetails?.schoolName}</p>
@@ -254,21 +256,32 @@ export async function POST(
       <p> Coordinator Cell: ${schoolDetails?.c_contact}</p>
       <p> Coordinator Account Details: ${schoolDetails?.c_accountDetails}</p>
       <p> Total Number of students Registered: ${totalStudents.length}</p>
-      ${tableHtml}`,
+      ${tableHtml}
+
+      <p>Best Regards</p>
+      
+      <p><b>Team IKMC</b></p>
+      <p>Innovative Learning - KSF Pakistan</p>
+      <p><b>Office: </b> 042-37180505 | 042-37180506</p>
+      <p><b>Whatsapp: </b>0333-2111399 | 0321-8403033 | 0319-5080077</p>
+      <p><b>Address: </b>1st Floor, Plaza 114, Main Boulevard, Phase 6, D.H.A Lahore</p>
+      <a href="www.kangaroopakistan.org" target="#">www.kangaroopakistan.org</a>
+      
+      `,
     };
 
-    // try {
-    //   await emailManager.sendEmail(mailOptions);
-    //   return NextResponse.json("Email sent Successfully", { status: 200 });
-    // } catch (error) {
-    //   console.error("Failed to send email:", error);
-    //   return NextResponse.json(
-    //     { message: "Failed to send email" },
-    //     { status: 500 }
-    //   );
-    // }
+    try {
+      await emailManager.sendEmail(mailOptions);
+      return NextResponse.json("Email sent Successfully", { status: 200 });
+    } catch (error) {
+      console.error("Failed to send email:", error);
+      return NextResponse.json(
+        { message: "Failed to send email" },
+        { status: 500 }
+      );
+    }
 
-    return NextResponse.json(createdStudents, { status: 201 });
+    // return NextResponse.json(createdStudents, { status: 201 });
   } catch (error) {
     console.error("Request error", error);
     return NextResponse.json(
