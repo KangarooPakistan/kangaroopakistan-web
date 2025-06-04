d4f9f632e6abb2000ed72496e22bf7a1d92043c5
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-06-04 20:18:06 +0500

    update text

--- app/api/users/sendemail/[registrationId]/route.ts
+++ app/api/users/sendemail/[registrationId]/route.ts
@@ -183,7 +183,7 @@ export async function GET(
        <p>Best Regards</p>
       
       <p><b>Team ${contestNameShort}</b></p>
-      <p>Innovative Learning - KSF Pakistan</p>
+      <p>Inventive Learning</p>
       <p><b>Office: </b> 042-37180505 | 042-37180506</p>
       <p><b>Whatsapp: </b>0333-2111399 | 0321-8403033 | 0319-5080077</p>
       <p><b>Address: </b>1st Floor, Plaza 114, Main Boulevard, Phase 6, D.H.A Lahore</p>
