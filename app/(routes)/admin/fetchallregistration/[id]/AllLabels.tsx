f862ed6d581296ebe7563d7ba4552cb1b62fec6e
kainat <kainat@lcisystems.com>
2025-09-29 15:38:58 +0500

    `Update "Innovative Learning" to "INVENTIVE LEARNING" in AllLabels component`

--- app/(routes)/admin/fetchallregistration/[id]/AllLabels.tsx
+++ app/(routes)/admin/fetchallregistration/[id]/AllLabels.tsx
@@ -142,7 +142,7 @@ const AllLabels: React.FC<AllLabelsProps> = ({ schoolDetails }) => {
 
           <View style={styles.fromBlock}>
             <Text style={[styles.text, styles.bold]}>From</Text>
-            <Text style={[styles.text, styles.bold]}>Innovative Learning </Text>
+            <Text style={[styles.text, styles.bold]}>INVENTIVE LEARNING </Text>
             <Text style={styles.subText}>
               1st FLOOR, PLAZA 114, MAIN BOULEVARD, PHASE 6 DHA, LAHORE{" "}
             </Text>
